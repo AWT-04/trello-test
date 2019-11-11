f9bd80d0caab2d38556dca16f56dd9fb1373bc16
FerHinojosa <fernando.hinojosa@live.com>
2019-11-11 16:29:49 -0400

    Basic files (#4)
    * Basic files
    
    * Added basic test

---
+++ src/test/java/org/fundacionjala/trello/LibraryTest.java
@@ -0,0 +1,19 @@
+package org.fundacionjala.trello;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Test for {@link Library}.
+ */
+public class LibraryTest {
+
+    /**
+     * Verify some library method.
+     */
+    @Test
+    public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    }
+}
