a9cb249a7804c0ca6686f3e3aeb47f90e97b3594
ramalaso <osalamar@gmail.com>
2019-11-13 15:55:07 -0400

    Added more  cards tests

--- src/test/java/org/fundacionjala/trello/runner/Runner.java
+++ src/test/java/org/fundacionjala/trello/runner/Runner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"org.fundacionjala.trello"}
+        glue = {"org.fundacionjala.trello.steps"}
 )
 
 public class Runner extends AbstractTestNGCucumberTests {
