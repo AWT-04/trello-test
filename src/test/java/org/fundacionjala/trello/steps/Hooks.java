287bdacd997b6398437c51f414294149e947849b
Andy Bazualdo Quevedo <AndyBazualdo@Fundacion-jala.local>
2019-11-21 11:27:24 -0400

    fix hooks

--- src/test/java/org/fundacionjala/trello/steps/Hooks.java
+++ src/test/java/org/fundacionjala/trello/steps/Hooks.java
@@ -10,20 +10,21 @@ import org.fundacionjala.core.utils.ConfigVariableHandler;
 import java.util.List;
 
 public class Hooks {
+    private final String ACCESS = "owner";
 
     @After("@cleanTeams")
     public void iSendDeleteAllTeamsByPrefixBefore() {
         ConfigVariableHandler configVariableHandler = new ConfigVariableHandler();
         final String prefix = "test";
-        Response response = RequestManager.get(Authentication.getRequestSpecification("owner"),
+        Response response = RequestManager.get(Authentication.getRequestSpecification(ACCESS),
                 "/members/me/organizations?fields=all)");
         List<Integer> allID = response.jsonPath().getList("id");
         for (int i = 0; i < allID.size(); i++) {
-            String name = RequestManager.get(Authentication.getRequestSpecification("owner"),
+            String name = RequestManager.get(Authentication.getRequestSpecification(ACCESS),
                     "/organizations/" + allID.get(i)).jsonPath().getString("name");
             System.out.println(name.contains(prefix.toLowerCase()));
             if (name.contains(prefix.toLowerCase())) {
-                RequestManager.delete(Authentication.getRequestSpecification("owner"),
+                RequestManager.delete(Authentication.getRequestSpecification(ACCESS),
                         "/organizations/" + allID.get(i));
             }
         }
@@ -33,13 +34,13 @@ public class Hooks {
     public void iSendDeleteAllBoardsByPrefixBefore() {
         ConfigVariableHandler configVariableHandler = new ConfigVariableHandler();
         final String prefix = " ";
-        Response response = RequestManager.get(Authentication.getRequestSpecification("owner"),
+        Response response = RequestManager.get(Authentication.getRequestSpecification(ACCESS),
                 "/members/me/boards?filter=open)");
         List<String> allName = response.jsonPath().getList("name");
         List<Integer> allID = response.jsonPath().getList("id");
         for (int i = 0; i < allName.size(); i++) {
             if (allName.get(i).contains(prefix.toLowerCase())) {
-                RequestManager.delete(Authentication.getRequestSpecification("owner"),
+                RequestManager.delete(Authentication.getRequestSpecification(ACCESS),
                         "/boards/" + allID.get(i));
             }
         }
@@ -49,13 +50,13 @@ public class Hooks {
     public void iSendDeleteAllCardsToByPrefixBefore() {
         ConfigVariableHandler configVariableHandler = new ConfigVariableHandler();
         final String prefix = " ";
-        Response response = RequestManager.get(Authentication.getRequestSpecification("owner"),
+        Response response = RequestManager.get(Authentication.getRequestSpecification(ACCESS),
                 "/members/me/cards?filter=open)");
         List<String> allName = response.jsonPath().getList("name");
         List<Integer> allID = response.jsonPath().getList("id");
         for (int i = 0; i < allName.size(); i++) {
             if (allName.get(i).contains(prefix.toLowerCase())) {
-                RequestManager.delete(Authentication.getRequestSpecification("owner"),
+                RequestManager.delete(Authentication.getRequestSpecification(ACCESS),
                         "/cards/" + allID.get(i));
             }
         }
@@ -65,14 +66,14 @@ public class Hooks {
     public void iSendDeleteAllBoardsByPrefixAfter() {
         ConfigVariableHandler configVariableHandler = new ConfigVariableHandler();
         final String prefix = "Fernando";
-        Response response = RequestManager.get(Authentication.getRequestSpecification("owner"),
-                "/members/me/boards?filter=open)&key=" + configVariableHandler.getKeyToken("owner")
-                        .get("keyToken") + "&token=" + configVariableHandler.getApiToken("owner").get("apiToken"));
+        Response response = RequestManager.get(Authentication.getRequestSpecification(ACCESS),
+                "/members/me/boards?filter=open)&key=" + configVariableHandler.getKeyToken(ACCESS)
+                        .get("keyToken") + "&token=" + configVariableHandler.getApiToken(ACCESS).get("apiToken"));
         List<String> allName = response.jsonPath().getList("name");
         List<Integer> allID = response.jsonPath().getList("id");
         for (int i = 0; i < allName.size(); i++) {
             if (allName.get(i).contains(prefix)) {
-                RequestManager.delete(Authentication.getRequestSpecification("owner"),
+                RequestManager.delete(Authentication.getRequestSpecification(ACCESS),
                         "/boards/" + allID.get(i));
             }
         }
@@ -82,13 +83,13 @@ public class Hooks {
     public void iSendDeleteAllCardsToByPrefixAfter() {
         ConfigVariableHandler configVariableHandler = new ConfigVariableHandler();
         final String prefix = " ";
-        Response response = RequestManager.get(Authentication.getRequestSpecification("owner"),
+        Response response = RequestManager.get(Authentication.getRequestSpecification(ACCESS),
                 "/members/me/cards?filter=open)");
         List<String> allName = response.jsonPath().getList("name");
         List<Integer> allID = response.jsonPath().getList("id");
         for (int i = 0; i < allName.size(); i++) {
             if (allName.get(i).contains(prefix.toLowerCase())) {
-                RequestManager.delete(Authentication.getRequestSpecification("owner"),
+                RequestManager.delete(Authentication.getRequestSpecification(ACCESS),
                         "/cards/" + allID.get(i));
             }
         }
